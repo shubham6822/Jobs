56c1b7dd3039a5850598f8c58a70819fb18c7374
shubham6822 <sharmashubham6822@gmail.com>
2024-08-20 16:52:18 +0530

    header

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+  const router  = useRouter()
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Shubham</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
     </View>
   )
 }
