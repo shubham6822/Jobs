effae1e238e1d7c04ff78fd9ddb961003408df8a
shubham6822 <sharmashubham6822@gmail.com>
2024-08-26 16:04:11 +0530

    popularjobs-card

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 import {
   View,
@@ -11,12 +11,48 @@ import {
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFecth";
+import axios from "axios";
 
 const Popularjobs = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [selectedJob, setSelectedJob] = useState();
+  const [jobs, setJobs] = useState([]);
+
+  const fetchjobs = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://jsearch.p.rapidapi.com/search',
+      params: {
+        query: "React developer",
+        num_pages: "1",
+      },
+      headers: {
+        'x-rapidapi-key': 'f098f2b966mshdda4ba10129c67fp1eaa18jsn1e7f04676b51',
+        'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+      }
+    };
+
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setJobs(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+      console.error(error);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchjobs();
+  }
+    , []);
+
 
   return (
     <View style={styles.container}>
@@ -34,10 +70,12 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5]}
+            data={jobs}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={(item) => item.job_id}
---
+++ hook/useFecth.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = async (endpoint, query) => {
+
+};
+
+export default useFetch;
